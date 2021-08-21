853325e20e8ef5817ced6f9721b05a77b4740a52
brunoBMonteiro <brunobilheri@hotmail.com>
2021-08-20 22:27:52 -0300

    Adding Entity to Database

--- src/main/java/com/example/spring/SpringAddressJpaApplication.java
+++ src/main/java/com/example/spring/SpringAddressJpaApplication.java
@@ -1,9 +1,12 @@
 package com.example.spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-
+import com.example.spring.entity.AddressEntity;
+import com.example.spring.repository.AddressRepository;
 
 @SpringBootApplication
 public class SpringAddressJpaApplication {
@@ -12,4 +15,13 @@ public class SpringAddressJpaApplication {
 		SpringApplication.run(SpringAddressJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(AddressRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new AddressEntity("90010-170", "Praça Montevidéo"));
+			repository.save(new AddressEntity("94935-410", "Lidio Batista Soares"));
+			repository.save(new AddressEntity("01001-000", "Praça da Sé"));
+		};
+	}
 }
