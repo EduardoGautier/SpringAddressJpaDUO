c41b8ea1f339ed6c08645ff80c60c0f49bb0a166
Eduardo Gautier <duduchn2011@gmail.com>
2021-08-20 21:47:25 -0300

    Folder structure

---
+++ src/main/java/com/example/spring/SpringCepJpaApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringCepJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCepJpaApplication.class, args);
+	}
+
+}
