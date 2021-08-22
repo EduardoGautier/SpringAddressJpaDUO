0aadbeed4747584cfadaf314800342531e1e3465
brunoBMonteiro <brunobilheri@hotmail.com>
2021-08-21 21:12:54 -0300

    Adding de-serializer and serializer

---
+++ src/main/java/com/example/spring/controller/JacksonCustomAddressEntityDeserializer.java
@@ -0,0 +1,49 @@
+package com.example.spring.controller;
+
+
+import java.io.IOException;
+
+import com.example.spring.entity.AddressEntity;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+public class JacksonCustomAddressEntityDeserializer extends StdDeserializer<AddressEntity> {
+	
+	
+	
+
+	public JacksonCustomAddressEntityDeserializer(){
+		this(null);
+	}
+
+	public JacksonCustomAddressEntityDeserializer(Class<AddressEntity> t) {
+		super(t);
+	}
+	
+	@Override
+	public AddressEntity deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
+		
+		JsonNode node = parser.getCodec().readTree(parser);
+		
+		AddressEntity address = new AddressEntity();
+		
+		String zipCode = node.get("cep").asText(null);
+		
+		String publicPlace = node.get("logradouro").asText(null);
+		
+		
+		address.setzipCode(zipCode);
+		
+		address.setpublicPlace(publicPlace);
+		
+		return address;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/example/spring/controller/JacksonCustomAddressEntitySerializer.java
@@ -0,0 +1,36 @@
+package com.example.spring.controller;
+
+
+import java.io.IOException;
+
+import com.example.spring.entity.AddressEntity;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class JacksonCustomAddressEntitySerializer extends StdSerializer<AddressEntity> {
+	
+
+	public JacksonCustomAddressEntitySerializer() {
+		this(null);
+	}
+	
+	public JacksonCustomAddressEntitySerializer(Class<AddressEntity> t) {
+		super(t);
+	}
+	
+	@Override
+	public void serialize(AddressEntity address, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+		
+		jgen.writeStartObject();
+		if (address.getId() == null) {
+			jgen.writeNullField("id");
+		} else {
+			jgen.writeNumberField("id", address.getId());
+		}
+
+		jgen.writeStringField("cep", address.getzipCode());
+		jgen.writeStringField("logradouro", address.getpublicPlace());
+		
+	}
+}
